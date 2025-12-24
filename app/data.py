dbe09898f80bf76a95f7df7b5479b04916fbff7f
joshkykartoshky <gallejohannes10@gmail.com>
2025-12-24 18:42:48 +0100

    Create data.py

---
+++ app/data.py
@@ -0,0 +1,17 @@
+companies = {
+    "demo": {
+        "name": "Demo Firma",
+        "address": "Musterstraße 1, 88499",
+        "phone": "07500 123456",
+        "email": "info@demo-firma.de",
+        "opening_hours": "Mo–Fr 09:00–17:00",
+        "services": "Beratung, Service, Support"
+    }
+}
+
+knowledge = """
+Wenn Kunden etwas nicht finden:
+- Öffnungszeiten nennen
+- Telefonnummer nennen
+- Leistungen erklären
+"""
